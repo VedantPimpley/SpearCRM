bf84c5c12ecbe17952d04aacaee26a4fd732e7de
VedantPimpley <vedantpimpley7@gmail.com>
2020-04-15 15:20:09 +0530

    Updated PrettyList to allow dynamic component rendering

--- src/components/subcomponents/PrettyList.js
+++ src/components/subcomponents/PrettyList.js
@@ -5,17 +5,35 @@ export default function PrettyList() {
   return(
     <div className="pretty-list">
       <ul class="experiences">
-        <li class="blue">
-          <div class="where">Past Event/Task</div>
-          <div class="when">2020-04-01</div>
-          <p class="description">Signed $10k deal</p>
-         </li>
-        <li class="green">
-          <div class="where">Logged call</div>
-          <div class="when">2020-03-01</div>
-          <p class="description">Talked business</p>
-         </li>
+        {sample_activity.map( (element, i) => {
+          return (
+            <li class="blue" key={i}>
+              <div class="where"> {element.activityTitle} </div>
+              <div class="when"> {element.activityData} </div>
+              <p class="description"> {element.activityBody} </p>
+           </li>
+          );
+        })
+        }
       </ul>
     </div>
   );
 }
+
+const sample_activity = [
+  {
+    activityTitle: "Past Event/Task",
+    activityData: "2020-04-01",
+    activityBody: "Signed $10k deal",
+  },
+  {
+    activityTitle: "Logged call",
+    activityData: "2020-03-01",
+    activityBody: "Talked business",
+  },
+  {
+    activityTitle: "Sent email",
+    activityData: "2020-04-04",
+    activityBody: "Sent prospective deal",
+  },
+];
