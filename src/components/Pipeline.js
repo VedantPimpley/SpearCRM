78982402d3c1fca572a5c54679cbca5a3d32e19a
VedantPimpley <vedantpimpley7@gmail.com>
2020-05-03 10:54:52 +0530

    Added pipeline with logic

--- src/components/Pipeline.js
+++ src/components/Pipeline.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import Board from 'react-trello/dist'
-import Button from "@material-ui/core/Button";
-import SaveIcon from '@material-ui/icons/Save';
-import './styles/Pipeline.css'
 
 //draggable is false for all orders in lane Lane 4 - archived orders
 
 export default class Pipeline extends React.Component {
   state = {
     fetchedOrders : [],
-    unsavedBoardData : {}
   };
 
   componentDidMount() {
+    //API Call to get orders. Here repr. by sampleAPIData
+    const sampleAPIData = [
+      {"_id": {"$oid": "5ea593ecd98710bdcd066182"}, "account_name": "Jake Paul", "company": "Google", "no_of_shares": 40, "cost_of_share": 20, "stage": 1,
+      "account_id": "5ea58fbc63e50fc607cf6a12", "trans_type": "sell"},
+      {"_id": {"$oid": "5ea593f6d98710bdcd066183"}, 
+      "company": "Google", "no_of_shares": 40, "account_name": "Jeff Paul", "cost_of_share": 20, "stage": 2, "account_id": "5ea58fbc63e50fc607cf6a12",
+      "trans_type": "sell"},
+    ];
+    this.setState({ fetchedOrders: sampleAPIData });
+  }
+
+  updatePipelineAPICall() {
     //API Call to get orders. Here repr. by sampleAPIData
     const sampleAPIData = [
       {"_id": {"$oid": "5ea593ecd98710bdcd066182"}, "account_name": "Jake Paul", "company": "Google", "no_of_shares": 40, "cost_of_share": 20, "stage": 1,
@@ -24,33 +32,29 @@ export default class Pipeline extends React.Component {
     this.setState({ fetchedOrders: sampleAPIData });
   }
 
-  componentDidUpdate() {
-    console.log(this.state.unsavedBoardData);
-  }
-
   transformOrdersToBoardData = (orders) => {
     const board = {
       lanes: [
         {
-          id: 'lane1',
+          id: 1,
           title: 'Negotiating order',
           label: '1',
           cards: []
         },
         {
-          id: 'lane2',
+          id: 2,
           title: 'Finalized order',
           label: '2',
           cards: []
         },
         {
-          id: 'lane3',
+          id: 3,
           title: 'Transacted order',
           label: '3',
           cards: []
         },
         {
-          id: 'lane4',
+          id: 0,
           title: 'Archived order',
           label: '4',
           cards: []
@@ -80,28 +84,48 @@ export default class Pipeline extends React.Component {
     return board;
   }
 
-  setUnsavedBoardData = (newData) => {
-    // this.setState({ unsavedBoardData: newData });
-    return null;
+  updateCardStage = async (fromLaneId, toLaneId, cardId, index) => {
+    const newCardStage = {
+      "order_id" : cardId,
+      "stage" : toLaneId
+    };
+
+    console.log(newCardStage);
+
+    const response = await fetch("/main/order_stage_change", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newCardStage)
+    });
+    
+    if (response.ok) {
+      console.log("response worked!");
+      console.log(response);
+      this.updatePipelineAPICall();
+    }
   }
 
-  postUpdatedBoardData = () => {
-    return null;
+  deleteCard = async (cardId, laneId) => {
+    console.log(cardId);
+    fetch(`main/delete_order/${cardId}`).then(response =>
+      response.json().then(data => {
+        if (data==="order deleted"){
+          this.updatePipelineAPICall();
+        }
+      })
+    );
   }
 
   render() {
     return(
       <>
         <Board
-           data={this.transformOrdersToBoardData(this.state.fetchedOrders)}
-          onDataChange={this.setUnsavedBoardData}
+          data={this.transformOrdersToBoardData(this.state.fetchedOrders)}
+          onCardDelete={this.deleteCard}
+          onCardMoveAcrossLanes={this.updateCardStage}
         />
-        <div className="save-changes-button"> 
-          <Button variant="outlined" style={{ backgroundColor: "#31ba71"}} onClick={this.postUpdatedBoardData}>
-            <SaveIcon />
-            Save Changes
-          </Button> 
-        </div>
       </>
     );
   }
