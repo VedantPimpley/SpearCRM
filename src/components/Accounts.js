e3c9d7f4fcba305e3f8fbf03d1bc6724390cf5cf
VedantPimpley <vedantpimpley7@gmail.com>
2020-06-06 17:52:54 +0530

    Tackling CORS policy fetch error

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -41,7 +41,7 @@ export default class Accounts extends React.Component {
   componentDidMount() {
     this._isMounted = true;
 
-    fetch(`${API}/main/show_all_accounts`).then(response =>
+    fetch(`https://cors-anywhere.herokuapp.com/${API}/main/show_all_accounts`).then(response =>
       response.json().then(data => {
         if (this._isMounted) {
           this.setState({ fetchedData: data });
@@ -55,7 +55,7 @@ export default class Accounts extends React.Component {
   }
 
   updateAccountsAPICall = () => {
-    fetch(`${API}//main/show_all_accounts`).then(response =>
+    fetch(`${API}/main/show_all_accounts`).then(response =>
       response.json().then(data => {
         if (this._isMounted) {
           this.setState({ fetchedData: data });
